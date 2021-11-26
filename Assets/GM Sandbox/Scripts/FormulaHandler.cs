9c50096b9b262f4bc628f468defb3eb002ea83d1
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2021-11-26 11:51:48 +0000

    Formula UI Panel Complete

--- Assets/GM Sandbox/Scripts/FormulaHandler.cs
+++ Assets/GM Sandbox/Scripts/FormulaHandler.cs
@@ -3,13 +3,30 @@ using UnityEngine.UI;
 
 public class FormulaHandler : MonoBehaviour
 {
-	[Header("Parameters")]
-	[SerializeField] private int sideLength = 1;
-
-	[Header("Object References")]
+	[Header("General Parameters")]
 	[SerializeField] private GameObject formulaPanel = default;
-	[SerializeField] private TextDisplay lengthText = default;
+	[SerializeField] private GameObject selectionPanel = default;
+	[SerializeField] private GameObject backButton = default;
 	[SerializeField] private TextDisplay totalAreaText = default;
+
+	[Header("Equilateral Parameters")]
+	[SerializeField] private GameObject equilateralPanel = default;
+	[SerializeField] private int sideLength = 1;
+	[SerializeField] private TextDisplay sideText = default;
+
+	[Header("Isosceles Parameters")]
+	[SerializeField] private GameObject isoscelesPanel = default;
+	[SerializeField] private int baseLength = 1;
+	[SerializeField] private TextDisplay baseText = default;
+	[SerializeField] private int heightLength = 1;
+	[SerializeField] private TextDisplay heightText = default;
+
+	[Header("Rectangle Parameters")]
+	[SerializeField] private GameObject rectanglePanel = default;
+	[SerializeField] private int widthLength = 1;
+	[SerializeField] private TextDisplay widthText = default;
+	[SerializeField] private int rectangleLength = 1;
+	[SerializeField] private TextDisplay lengthText = default;
 
 	private Field currentField;
 	private int totalArea;
@@ -22,7 +39,7 @@ public class FormulaHandler : MonoBehaviour
 
 	private void UpdateFormulaDisplay()
 	{
-		lengthText.SetTextToFloat(sideLength);
+		sideText.SetTextToFloat(sideLength);
 		totalArea = (int)Mathf.Round((Mathf.Sqrt(3) / 4) * sideLength * sideLength);
 		totalAreaText.SetTextToFloat(totalArea);
 	}
@@ -31,12 +48,55 @@ public class FormulaHandler : MonoBehaviour
 	{
 		currentField = field;
 		formulaPanel.SetActive(true);
-		UpdateFormulaDisplay();
+		ToggleSelectionPanel();
 	}
 
 	public void ClosePanel()
 	{
 		currentField = null;
 		formulaPanel.SetActive(false);
+	}
+
+	public void ToggleEquilateralPanel()
+	{
+		selectionPanel.SetActive(false);
+		equilateralPanel.SetActive(true);
+		isoscelesPanel.SetActive(false);
+		rectanglePanel.SetActive(false);
+		backButton.SetActive(true);
+		totalAreaText.transform.parent.gameObject.SetActive(true);
+		UpdateFormulaDisplay();
+	}
+
+	public void ToggleIsoscelesPanel()
+	{
+		selectionPanel.SetActive(false);
+		equilateralPanel.SetActive(false);
+		isoscelesPanel.SetActive(true);
+		rectanglePanel.SetActive(false);
+		backButton.SetActive(true);
+		totalAreaText.transform.parent.gameObject.SetActive(true);
+		UpdateFormulaDisplay();
+	}
+
+	public void ToggleRectanglePanel()
+	{
+		selectionPanel.SetActive(false);
+		equilateralPanel.SetActive(false);
+		isoscelesPanel.SetActive(false);
+		rectanglePanel.SetActive(true);
+		backButton.SetActive(true);
+		totalAreaText.transform.parent.gameObject.SetActive(true);
+		UpdateFormulaDisplay();
+	}
+
+	public void ToggleSelectionPanel()
+	{
+		selectionPanel.SetActive(true);
+		equilateralPanel.SetActive(false);
+		isoscelesPanel.SetActive(false);
+		rectanglePanel.SetActive(false);
+		backButton.SetActive(false);
+		totalAreaText.transform.parent.gameObject.SetActive(false);
 	}
 }
