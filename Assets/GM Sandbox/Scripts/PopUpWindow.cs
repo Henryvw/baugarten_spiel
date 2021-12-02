d5d05e4c3ba8d8defa9198c9ff3d8bf0c0947fe5
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2021-12-02 16:13:44 +0000

    Freeze Time on Pop Up

--- Assets/GM Sandbox/Scripts/PopUpWindow.cs
+++ Assets/GM Sandbox/Scripts/PopUpWindow.cs
@@ -4,6 +4,15 @@ using UnityEngine.UI;
 public class PopUpWindow : MonoBehaviour
 {
     [SerializeField] private Text mainText = default;
+    [SerializeField] private bool freezeTimeOnOpen = true;
+
+    private void Start()
+    {
+        if (freezeTimeOnOpen)
+        {
+            Time.timeScale = 0f;
+        }
+    }
 
     public void SetMainTextTo(string newText)
     {
@@ -12,6 +21,11 @@ public class PopUpWindow : MonoBehaviour
 
     public void OnClose()
     {
+        if (freezeTimeOnOpen)
+        {
+            Time.timeScale = 1f;
+        }
+        
         Destroy(this.gameObject);
     }
 }
