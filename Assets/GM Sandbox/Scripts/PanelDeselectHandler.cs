876a5d0c3e160ec3e7a7a97627ea4ecebbfa36b4
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2021-12-16 13:49:30 +0000

    Made DeselectHandler optional

--- Assets/GM Sandbox/Scripts/PanelDeselectHandler.cs
+++ Assets/GM Sandbox/Scripts/PanelDeselectHandler.cs
@@ -3,10 +3,13 @@ using UnityEngine.EventSystems;
 
 public class PanelDeselectHandler : MonoBehaviour, IDeselectHandler
 {
+    [SerializeField] private bool isActive = true;
     [SerializeField] private MenuHandler menuHandler = default;
 
     public void OnDeselect(BaseEventData data)
     {
+        if (!isActive) { return; }
+
         menuHandler.ClosePanels();
     }
 }
