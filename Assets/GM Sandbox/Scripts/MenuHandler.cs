9636d0b93859e2831a5a06bbf94914ca39addd68
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2021-12-16 11:56:37 +0000

    Bug Fix: Panel Idle Alpha 1

--- Assets/GM Sandbox/Scripts/MenuHandler.cs
+++ Assets/GM Sandbox/Scripts/MenuHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MenuHandler : MonoBehaviour
 {
@@ -53,6 +54,7 @@ public class MenuHandler : MonoBehaviour
         if (targetPanel.activeSelf)
         {
             StartCoroutine(DisablePanel(targetPanel));
+            EventSystem.current.SetSelectedGameObject(null);
         }
         else
         {
